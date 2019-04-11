a01678ab0edb12a614efb1f88cd40522d3d62576
baonguyen1511 <49017095+baonguyen1511@users.noreply.github.com>
2019-04-11 14:17:49 +0700

    Add project files.

---
+++ BigSchool/ViewModels/CoursesViewModel.cs
@@ -0,0 +1,12 @@
+﻿using BigSchool.Models;
+using System.Collections.Generic;
+
+namespace BigSchool.ViewModels
+{
+    public class CoursesViewModel
+    {
+        public IEnumerable<Course> UpcommingCourses { get; set; }
+        public bool ShowAction { get; set; }
+        public IEnumerable<ApplicationUser> UpcommingFollowing { get; set; }
+    }
+}
